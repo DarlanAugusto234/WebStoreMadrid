2f0eb7f6ac918a607b573b668b3e8ba76cc7a75d
Darlan Augusto <darlanaugusto234@gmail.com>
2024-05-25 23:47:54 -0300

    Add Comentario Especial

--- src/main/java/com/Madrid/WebStore/Service/HomeService.java
+++ src/main/java/com/Madrid/WebStore/Service/HomeService.java
@@ -26,7 +26,7 @@ public class HomeService {
         this.modelMapper = modelMapper;
     }
 
-    // COMENTAR AQUI
+    // Metodo de Barra de Pesquisa
     public List<ProdutoDTO> pesquisarProdutos(String query) {
         // Usar um Set para evitar duplicatas
         Set<Produto> produtos = new HashSet<>(produtoRepositorio.findByNomeProdutoContainingIgnoreCase(query));
@@ -57,7 +57,24 @@ public class HomeService {
         return produtosDTO;
     }
 
-    // Método para buscar um produto pelo ID
+    // Codigo para Teste de Segunda Feira
+    /*
+    public List<ProdutoResumoDTO> listarProdutosDestaque() {
+        List<Produto> produtos = produtoRepositorio.findByDestaqueTrue();
+        List<ProdutoResumoDTO> produtosDTO = new ArrayList<>();
+        for (Produto produto : produtos) {
+            ProdutoResumoDTO produtoResumoDTO = new ProdutoResumoDTO();
+            produtoResumoDTO.setNomeProduto(produto.getNomeProduto());
+            produtoResumoDTO.setCategoria(produto.getCategoria().getNomeCategoria());
+            produtoResumoDTO.setValor(produto.getValor());
+            produtoResumoDTO.setDescricao(produto.getDescricao());
+            produtosDTO.add(produtoResumoDTO);
+        }
+        return produtosDTO;
+    }
+    */
+
+    // Método para buscar um produto em destaque pelo ID
     public ProdutoResumoDTO buscarProdutoPorId(Integer id) {
         Produto produto = produtoRepositorio.findById(id).orElseThrow();
 
